be97b8dacf21f81b21691b73344e984e6d91654f
Terry Magnus Drever <terrymagnusdrever@wellfired.com>
2017-02-04 20:40:15 +0000

    Attempt to force TC Build.

--- Assets/WellFired.BuildSystem/Editor/DoABuild.cs
+++ Assets/WellFired.BuildSystem/Editor/DoABuild.cs
@@ -10,6 +10,6 @@ public static class DoABuild
         if (Application.platform == RuntimePlatform.OSXEditor)
             target = BuildTarget.StandaloneOSXUniversal;
 
-        BuildPipeline.BuildPlayer(new[] {"Assets/scenes/main.unity"}, "builds/build", target, BuildOptions.None);
+        BuildPipeline.BuildPlayer(new[] { "Assets/scenes/main.unity" }, "builds/build", target, BuildOptions.None);
     }
 }
