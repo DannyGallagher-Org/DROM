b962e2a8477275462b9fe4c24ab3f2a93fdad6d0
Terry Magnus Drever <terrymagnusdrever@wellfired.com>
2017-02-04 18:09:53 +0000

    Kill the running app after a while.

---
+++ Assets/KillAfternSeconds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillAfternSeconds : MonoBehaviour 
+{
+	[SerializeField] private float _waitTime = 10.0f;
+
+	private void Start()
+	{
+		StartCoroutine(KillAfter());
+	}
+
+	private IEnumerator KillAfter()
+	{
+		yield return new WaitForSeconds(_waitTime);
+		Application.Quit();
+	}
+}
