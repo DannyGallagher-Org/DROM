e673d6e1994f661e4715d073e98d291710860bf4
Terry Magnus Drever <terrymagnusdrever@wellfired.com>
2017-02-04 18:31:53 +0000

    Really simple build system.

---
+++ Assets/WellFired.BuildSystem/Editor/DoABuild.cs
@@ -0,0 +1,15 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public static class DoABuild
+{
+    public static void DoIt()
+    {
+        var target = BuildTarget.StandaloneWindows;
+
+        if (Application.platform == RuntimePlatform.OSXEditor)
+            target = BuildTarget.StandaloneOSXUniversal;
+
+        BuildPipeline.BuildPlayer(new[] {"Assets/scenes/main.unity"}, "builds/build", target, BuildOptions.None);
+    }
+}
