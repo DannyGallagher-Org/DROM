c549fda82f4f3c4218f9ee5093a8f626e024a742
DannyG59 <danielgallagher.59@gmail.com>
2019-06-10 22:03:55 +0100

    whatever

--- Assets/Scripts/TextFade.cs
+++ Assets/Scripts/TextFade.cs
@@ -3,34 +3,44 @@
 // Copyright Funny Looking Games 2016
 ///
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
-using TMPro;
 
-public class TextFade : MonoBehaviour {
-	
+public class TextFade : MonoBehaviour
+{
+
+	public bool PlayOnAwake = false;
+	private bool _active = false;
 	private TextMeshProUGUI _textMeshProUgui;
+	public float Delay = 0f;
 	public float Speed = 2f;
 
 	public bool On;
 	
 	private void Awake()
 	{
+		if (PlayOnAwake)
+		{
+			hUtility.StaticCoroutinableObject.WaitForSecondsAsPromise(Delay)
+				.Then(o => { _active = true; });
+		}
+		
 		_textMeshProUgui = GetComponent<TextMeshProUGUI>();
 	}
 
 	private void Update()
 	{
-		_textMeshProUgui.alpha = Mathf.Lerp(_textMeshProUgui.alpha, On ? 1f : 0f, Time.deltaTime*Speed);
+		if(_active)
+			_textMeshProUgui.alpha = Mathf.Lerp(_textMeshProUgui.alpha, On ? 1f : 0f, Time.deltaTime*Speed);
 	}
 
 	public void FadeOn()
 	{
+		_active = true;
 		On = true;
 	}
 
 	public void FadeOff()
 	{
+		_active = true;
 		On = false;
 	}
 }
