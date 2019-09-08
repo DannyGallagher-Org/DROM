4af347a1f830bcd6b581cba42b5914fb6c079029
DannyGallagher-Org <contact@dannygallagher.org>
2019-09-08 16:29:44 +0100

    [cleanup] delete some stuff 3

--- Assets/Code/DromBeginning.cs
+++ Assets/Code/DromBeginning.cs
@@ -12,9 +12,6 @@ namespace Code
             {
                 cloud.SetActive(false);
             }
-
-            GameManager.audioManager.PlayGameplayMusic();
-            GameManager.audioManager.NextLevel();
         }
     }
 }
